2f0714f62b5458ae0b94212eecbe3464a4dcda7c
Глеб Останин <gleb.ostanin2014@yandex.ru>
2023-07-30 12:54:28 +0600

    Fixed recipe creation and filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
-from django_filters.rest_framework import (
-    FilterSet, filters, ModelMultipleChoiceFilter)
+from django_filters.rest_framework import (FilterSet,
+                                           ModelMultipleChoiceFilter, filters)
 
 from recipes.models import Recipe, Tag
 
--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework.permissions import BasePermission
 
-
 GUEST_METHODS = ('GET', 'POST', 'OPTIONS', 'HEAD')
 
 
